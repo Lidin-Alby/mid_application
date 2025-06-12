d116302db47f9c2ec02fb4f524fd3ef0a499e9b8
Lidin Alby <lidinalby@gmail.com>
2025-06-12 13:54:46 +0530

    flutter update and build

--- lib/ip_address.dart
+++ lib/ip_address.dart
@@ -1,3 +1,3 @@
-// String ipv4 = 'https://edumid.com/backend';
+String ipv4 = 'https://edumid.com/backend';
 // String ipv4 = 'http://localhost:3000/backend';
-String ipv4 = 'http://10.0.2.2:3000/backend';
+// String ipv4 = 'http://10.0.2.2:3000/backend';
